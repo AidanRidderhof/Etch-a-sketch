ec517a886e16f6e945fa249cf0be78d29dd289a6
Aidan Ridderhof <aidan.ridderhof@gmail.com>
2025-01-02 13:50:17 -0500

    recover

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # Etch-a-sketch
+An etch a sketch you can use in browser, simply hover your mouse over squares to color
---
+++ javascript.js
@@ -0,0 +1,9 @@
+const container = document.querySelector("#container");
+let square = document.createElement("div");
+square.classList.add("cell");
+
+for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+      container.appendChild(square);
+    }
+  }
